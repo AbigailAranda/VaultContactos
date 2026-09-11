0fef81e2ecd514ef3b49178416ca729d55e25791
Grisel Abigail Aranda Ojeda <griselabigaila@gmail.com>
2026-09-11 10:16:43 -0400

    Agregar EnsureCreated para inicializar la tabla de SQLite en Docker

--- Program.cs
+++ Program.cs
@@ -24,4 +24,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 app.Run();
